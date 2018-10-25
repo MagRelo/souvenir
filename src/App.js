e72911f5af86c863de8562dc4b2764c93f691871
Matt Lovan <magrelo404@gmail.com>
2018-10-25 12:20:33 -0400

    smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import scrollToComponent from 'react-scroll-to-component';
+
 import Controller from './Controller';
 
 import './App.css';
+
+const scrollOptions = {
+  duration: 700,
+  ease: 'inOutCube'
+};
 
 class App extends Component {
   constructor(props) {
@@ -15,8 +22,6 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {}
-
   render() {
     return (
       <div className="App">
@@ -25,32 +30,92 @@ class App extends Component {
             <nav>
               <ul>
                 <li>
-                  <a href="/#tokenize">Tokenize</a>
+                  <a
+                    href="/#tokenize"
+                    onClick={() => {
+                      scrollToComponent(
+                        this.tokenize,
+                        Object.assign(scrollOptions, { offset: 0 })
+                      );
+                    }}
+                  >
+                    Tokenize
+                  </a>
                 </li>
                 <li>
-                  <a href="/#distribute">Scatter</a>
+                  <a
+                    href="/#distribute"
+                    onClick={() => {
+                      scrollToComponent(
+                        this.distribute,
+                        Object.assign(scrollOptions, { offset: 0 })
+                      );
+                    }}
+                  >
+                    Scatter
+                  </a>
                 </li>
                 <li>
-                  <a href="/#contribute">Assemble</a>
+                  <a
+                    href="/#contribute"
+                    onClick={() => {
+                      scrollToComponent(
+                        this.contribute,
+                        Object.assign(scrollOptions, { offset: 0 })
+                      );
+                    }}
+                  >
+                    Assemble
+                  </a>
                 </li>
                 <li>
-                  <a href="/#performance">Create</a>
+                  <a
+                    href="/#create"
+                    onClick={() => {
+                      scrollToComponent(
+                        this.create,
+                        Object.assign(scrollOptions, { offset: 0 })
+                      );
+                    }}
+                  >
+                    Create
+                  </a>
                 </li>
                 <li>
-                  <a href="/#forge">Forge</a>{' '}
+                  <a
+                    href="/#forge"
+                    onClick={() => {
+                      scrollToComponent(
+                        this.forge,
+                        Object.assign(scrollOptions, { offset: 95 })
+                      );
+                    }}
+                  >
+                    Forge
+                  </a>
                 </li>
               </ul>
 
               <div>
-                <a href="/#">
-                  <span className="title">alloy</span>
+                <a
+                  href="/#"
+                  onClick={() => {
+                    scrollToComponent(this.home, scrollOptions);
+                  }}
+                >
+                  ><span className="title">alloy</span>
                 </a>
               </div>
             </nav>
           </div>
         </header>
 
-        <section id="home">
+        <section
+          id="home"
+          ref={section => {
+            this.home = section;
+          }}
+        >
           <div className="wrapper">
             <h1>Live Performance</h1>
 
@@ -96,30 +161,58 @@ class App extends Component {
           </div>
         </section>
 
-        <section id="tokenize" className=" grey">
+        <section
+          className="grey"
+          id="tokenize"
+          ref={section => {
+            this.tokenize = section;
+          }}
+        >
           <div className="wrapper">
             <h2>tokenize any digital media</h2>
             <p>test</p>
           </div>
         </section>
-        <section id="distribute">
+        <section
+          id="distribute"
+          ref={section => {
+            this.distribute = section;
+          }}
+        >
           <div className="wrapper">
             <h2>distribute your tokens</h2>
             <p>test</p>
           </div>
         </section>
-        <section id="contribute" className="grey">
+        <section
+          id="assemble"
+          className="grey"
+          ref={section => {
+            this.contribute = section;
+          }}
+        >
           <div className="wrapper">
             <h2>contribute</h2>
             <p>test</p>
           </div>
         </section>
-        <section id="performance">
+        <section
+          id="create"
+          ref={section => {
+            this.create = section;
+          }}
+        >
           <div className="wrapper">
             <Controller />
           </div>
         </section>
-        <section id="forge" className="grey">
+        <section
+          id="forge"
+          className="grey"
+          ref={section => {
+            this.forge = section;
+          }}
+        >
           <div className="wrapper">
             <h2>forge a unique performance artifact</h2>
             <p>The performance is now </p>
